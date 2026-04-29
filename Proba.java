1f0fc96ed9a39c70b886740c759873cbdb6b00d2
IvanZigolic <ivan.zigolic02@gmail.com>
2026-04-29 17:38:27 +0200

    Prva verzija koda

---
+++ Proba.java
@@ -0,0 +1,10 @@
+public class Proba {
+    public static void main (String[] args){
+	String Imena = "Marko \n" + "Matej \n" + "Ivan \n" + "Pero \n" + "Leon \n";
+		for(int i=1; i<=4; i++){
+		System.out.println(Imena);
+		}
+		System.out.println("\nZaposlenici:\n" + Imena);
+    }
+
+}
